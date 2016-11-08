658b6dad1eb799216e35c6884482a73f298e44e2
Kevin Hoffman <kehoffman@pivotal.io>
2016-11-08 15:28:24 -0500

    Adding placeholders for tests. Tests forthcoming.

---
+++ test/StatlerWaldorfCorp.EventProcessor.Tests/EventProcessorTest.cs
@@ -0,0 +1,26 @@
+using Xunit;
+using System;
+
+namespace StatlerWaldorfCorp.EventProcessor.Tests
+{
+    public class EventProcessorTest
+    {
+        [Fact]
+        public void EventProcessorDetectsTeamMemberProximities()
+        {
+            Assert.True(true); // TODO: add test
+        }
+
+        [Fact]
+        public void EventProcessorReturnsNoEventsForEmptyTeam()
+        {
+
+        }
+
+        [Fact]
+        public void EventProcessorReturnsNoEventsForOutsideDistanceThreshold()
+        {
+            
+        }
+    }
+}
