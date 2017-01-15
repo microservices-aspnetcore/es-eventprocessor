589eab4a05b96b6e5ec4957be6f6a40bd3955bf5
Kevin Hoffman <kehoffman@pivotal.io>
2017-01-15 08:56:41 -0500

    Preparing an AMQP subscriber implementation.

---
+++ src/StatlerWaldorfCorp.EventProcessor/Queues/AMQP/AMQPEventSubscriber.cs
@@ -0,0 +1,71 @@
+using System;
+using System.Text;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using StatlerWaldorfCorp.EventProcessor.Events;
+using Steeltoe.Extensions.Configuration.CloudFoundry;
+
+namespace StatlerWaldorfCorp.EventProcessor.Queues.AMQP
+{
+    public class AMQPEventSubscriber : AMQPClientBase, IEventSubscriber
+    {
+        private ILogger logger;
+        
+        private IConnection connection;
+
+        private EventingBasicConsumer consumer;
+        private string consumerTag;
+        private IModel channel;
+
+        public AMQPEventSubscriber(ILogger<AMQPEventSubscriber> logger,
+            IOptions<CloudFoundryServicesOptions> cfOptions,
+            IOptions<QueueOptions> queueOptions) : base(cfOptions, queueOptions)
+        {
+            this.logger = logger;
+
+            Initialize();             
+        }
+
+        private void Initialize()
+        {
+            connection = connectionFactory.CreateConnection();
+            channel = connection.CreateModel();
+
+            channel.QueueDeclare(
+                queue: queueOptions.MemberLocationRecordedEventQueueName,
+                durable: false,
+                exclusive: false,
+                autoDelete: false,
+                arguments: null
+            );
+
+            consumer = new EventingBasicConsumer(channel);
+
+            consumer.Received += (ch, ea) => {
+                var body = ea.Body;
+                var msg = Encoding.UTF8.GetString(body);
+                var evt = JsonConvert.DeserializeObject<MemberLocationRecordedEvent>(msg);
+                logger.LogInformation($"Received incoming event, {body.Length} bytes.");
+                if (MemberLocationRecordedEventReceived != null) {
+                    MemberLocationRecordedEventReceived(evt);
+                }
+                ((IModel)ch).BasicAck(ea.DeliveryTag, false);
+            };
+        }
+        
+        public event MemberLocationRecordedEventReceivedDelegate MemberLocationRecordedEventReceived;
+
+        public void Subscribe()
+        {
+            consumerTag = channel.BasicConsume(queueOptions.MemberLocationRecordedEventQueueName, false, consumer);       
+        }
+
+        public void Unsubscribe()
+        {
+            channel.BasicCancel(consumerTag);
+        }
+    }
+}
