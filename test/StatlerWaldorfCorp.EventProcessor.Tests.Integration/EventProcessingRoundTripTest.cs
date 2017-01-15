5c11e1cb9c7dc8c23e49f42df53cbc19f3eb01d1
Kevin Hoffman <kehoffman@pivotal.io>
2017-01-15 13:06:01 -0500

    Adding placeholder for integration tests

---
+++ test/StatlerWaldorfCorp.EventProcessor.Tests.Integration/EventProcessingRoundTripTest.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace StatlerWaldorfCorp.EventProcessor.Tests.Integration
+{
+    public class EventProcessingRoundTripTest
+    {
+        [Fact]
+        public void TestPlaceholder()
+        {
+            Assert.True(true);
+        }
+    }
+}
